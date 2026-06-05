53a05ac0f98ef753bae15fa0010decd7885f8cd1
Gagandeep <editoranonymous47@gmail.com>
2026-06-05 16:42:37 +0530

    looping

---
+++ Chapter 1/script.js
@@ -0,0 +1,27 @@
+// let a = 1
+
+// for (i = a ; i<11; i++){
+//     console.log(i)  
+// } 
+
+// let a = 10
+
+// while (a>0){
+//     console.log(a)
+//     a--
+// }
+
+//print even numbers from 0 to 20
+// for (let i = 0; i < 21; i++){
+//     if(i%2==0){
+//         console.log(i,"is even")
+//     }
+// }
+
+//print odd numbers from 0 to 15
+let i = 0
+while(i<15){
+    if(i%2!=0){
+        console.log(i,"is odd")
+    }
+}
