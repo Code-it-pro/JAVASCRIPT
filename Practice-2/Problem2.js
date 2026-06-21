d40664974050bae813e184919f362fdbfe354d4c
Gagandeep <editoranonymous47@gmail.com>
2026-06-21 18:43:04 +0530

    Update Problem2.js

--- Practice-2/Problem2.js
+++ Practice-2/Problem2.js
@@ -1,38 +1,27 @@
 function findSecondLargest(arr){
     if (arr.length < 0) return console.log("arr is empty");
-    if (arr.length == 1) return arr[0]
-    let max = 0;
-    let index;
-    let sub = [arr[0]]
-    for(let i = 0; i< arr.length; i++){
-        for( let j = 0; j< sub.length; j++){
-            if( sub[j] === arr[i]){
-                break;
+    if (arr.length === 1) return console.log("arr needs atleast 2 elements");
+    let max = -Infinity;
+    let secondMax = -Infinity;
+    let Newmax = -Infinity
+    for(let i = 0 ; i < arr.length ; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    for(let i = 0 ; i < arr.length ; i++){
+        if(arr[i] > Newmax){
+            Newmax = arr[i]
+            if( Newmax === max ){
+                Newmax = 0;
+            }else {
+                secondMax = arr[i]
             }
-            sub.push(arr[i])
         }
     }
-    console.log(sub)
-
-    for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] > max){
-            max = arr[i];
-            index = i
-        }
-    }
-    max = 0
-    for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] > max){
-            if( i === index) continue;
-            max = arr[i];
-            secondMax = i;
-        }
-    }
-    return arr[secondMax]
+    return secondMax
 }
 
 let arr = [10, 5, 20, 8, 20, 12]
 let secondlargestnum = findSecondLargest(arr)
 console.log(secondlargestnum)
-
-console.log("Here")
